4e6dbfaf4c68a32473b83110e3c9b0ef6ba8b4ad
aulas <aulas@SVP408D.academico.icesi.edu.co>
2022-10-05 07:44:40 -0500

    class 11

--- clase10/script.js
+++ clase10/script.js
@@ -1,3 +1,4 @@
+const mapContainer = document.getElementById("map-container")
 var listaDeTareas = []
 
 function createTaskList() {
@@ -23,4 +24,25 @@ function deleteLast() {
     createTaskList()
 }
 
-createTaskList()
+function createStudentMap(name, phone, age) {
+    const map = new Map()
+    map.set("nombre", name)
+    map.set("telefono", phone)
+    map.set("edad", age)
+    return map
+}
+
+function showDetailsInView(studentMap) {
+    var innerHtml = ""
+    innerHtml += "<label>Nombre: </label>"
+    innerHtml += "<p>" + studentMap.get("nombre") + "</p>"
+
+    innerHtml += "<label>Telefono: </label>"
+    innerHtml += "<p>" + studentMap.get("telefono") + "</p>"
+
+    innerHtml += "<label>Edad: </label>"
+    innerHtml += "<p>" + studentMap.get("edad") + "</p>"
+    mapContainer.innerHTML = innerHtml
+}
+const mapStudent = createStudentMap("Tiago", "301692...", 28)
+showDetailsInView(mapStudent)
