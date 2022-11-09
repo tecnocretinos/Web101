b2bcd52f2a12b78a189aad52413400ed10813d83
aulas <aulas@SVP408D.academico.icesi.edu.co>
2022-11-09 07:48:33 -0500

    Fetch

--- clase17/script.js
+++ clase17/script.js
@@ -1,10 +1,33 @@
 const fetchData = async () => {
+    const url = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=10'
     // Get the list from the api
-    const data = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await fetch(url)
     // Convert to json
     const dataJson = await data.json()
     // Print the results
-    console.log(dataJson.results)
+    console.log(dataJson)
+}
+
+const fetchOne = async () => {
+    const url = 'https://api.escuelajs.co/api/v1/products/10'
+    // Get the list from the api
+    const data = await fetch(url)
+    // Convert to json
+    const dataJson = await data.json()
+    // Print the results
+    console.log(dataJson)
+}
+
+function fetchAsync() {
+    const url = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=10'
+    fetch(url)
+        .then(response => {
+            response.json().then(data => {
+                console.log(data)
+            })
+        })
 }
 
 fetchData()
+fetchOne()
+//fetchAsync()
