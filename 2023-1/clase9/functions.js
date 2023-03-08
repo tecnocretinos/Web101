498566126ba4242a3ad274f35bc989acad9e8246
aulas <aulas@SVP408D.academico.icesi.edu.co>
2023-03-08 07:28:36 -0500

    Clase9

---
+++ 2023-1/clase9/functions.js
@@ -0,0 +1,13 @@
+function salduar(nombre) {
+    console.log("Hola " + nombre + "!")
+}
+
+let saludar2 = (nombre) => {
+    console.log("Hola " + nombre + "! 2")
+}
+
+function sumar(a,b) {
+    const c = a + b
+    const d = c/2
+    console.log(d)
+}
---
+++ 2023-1/clase9/index.js
@@ -0,0 +1,7 @@
+salduar("Tiago")
+saludar2("Camila")
+sumar(1,3)
+
+sumar(4,300)
+
+sumar(6797,3001)
