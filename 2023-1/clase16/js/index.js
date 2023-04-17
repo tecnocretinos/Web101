0db5c5a5cfb87c4c60e10a18ef92a562c4ba8f36
aulas <aulas@SVP408D.academico.icesi.edu.co>
2023-04-17 08:45:58 -0500

    Personajes parse

--- 2023-1/clase16/js/index.js
+++ 2023-1/clase16/js/index.js
@@ -5,3 +5,24 @@ for(let i = 0; i < data.length; i++){
     const personaje = new Personaje(obj.name, obj.image, obj.status, obj.species)
     contenedor.innerHTML += personaje.toHtml()
 }
+
+
+
+
+// Para el api de valorant
+let data2 = {
+    "personaje1": {
+        "key1": "value1",
+        "key2": "value2"
+    },
+    "personaje2": {
+        "key1": "value1",
+        "key2": "value2"
+    },
+}
+const keys = Object.keys(data2)
+for(let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    const obj = data2[key]
+    console.log(obj)
+}
