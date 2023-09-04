1c5e04f18a2411ab3bc333c0042ef2253181c29e
aulas <aulas@SVP408.academico.icesi.edu.co>
2023-09-04 08:02:00 -0500

    Js nit

---
+++ 2023-2/05-basics-javascript/index.js
@@ -0,0 +1,54 @@
+console.log("Inicio de la pagina")
+
+// Variables
+var primeraVar = 1
+let segundaVar = 2
+console.log(primeraVar)
+console.log(segundaVar)
+
+primeraVar = 123
+segundaVar = 13
+console.log(primeraVar)
+console.log(segundaVar)
+
+// Constantes
+const primeraConst = 9
+console.log(primeraConst)
+
+// Tipos de datos
+const numEntero = 1
+const numDeciales = 2.5
+const texto = "hola"
+const bool = true
+
+// Operaciones matematicas
+const num1 = 7
+const num2 = 9
+const num3 = 13
+const suma = num1 + num2 + num3
+const promedio = suma / 3
+console.log("El promedio es: " + promedio)
+
+const resta = num1 - num2 - num3
+console.log("La resta es: " + resta)
+
+const mult = num1 * num2 * num3
+console.log("La multiplicación es: " + mult)
+
+const newProm = (num1 + num2 + num3)/3
+console.log("El otro promedio es: " + newProm)
+
+// Concatenar strings (textos)
+const texto1 = "Hola"
+const texto2 = "Tiago"
+const texto3 = "!"
+const concatenado = texto1 + " " + texto2 + texto3
+console.log(concatenado)
+
+// Ojo
+const newVar = "1"
+const otherVar = 12
+const result = newVar + otherVar
+const result2 = otherVar + newVar
+console.log(result)
+console.log(result2)
