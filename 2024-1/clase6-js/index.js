d41ac0b0e0ab30445656bf1168b9b499089e0ebc
aulas <aulas@SVP408D.academico.icesi.edu.co>
2024-03-04 08:02:27 -0500

    Clase 6

---
+++ 2024-1/clase6-js/index.js
@@ -0,0 +1,51 @@
+var numeroUno = 98
+let numeroDos = 80
+const numeroTres = 105
+
+console.log(numeroUno)
+console.log(numeroDos)
+console.log(numeroTres)
+
+numeroUno = 100
+numeroDos = 90
+
+console.log(numeroUno)
+console.log(numeroDos)
+
+// Uso de operaciones matematicas
+const suma = 1 + 2 + 3 + numeroTres
+const multi = suma * 9
+const divi = multi/3
+const result = divi - 1
+console.log(result)
+
+// Esto es un comentario de una sola linea
+
+/*
+Esto tambien es un
+comentario de
+multiples lineas
+*/
+
+const sum = 5 + 4 + 3 + 1
+const total = 4
+const promedio = sum/total
+console.log(promedio)
+
+
+const textoUno = "Welcome to the Jungle"
+const textoDos = "We got fun and games"
+const textoCompleto = textoUno + " " + textoDos
+console.log(textoCompleto)
+
+
+// Operaciones logicas y comparaciones
+const boolUno = true
+const boolDos = true
+const boolResult = boolUno || boolDos
+console.log(boolResult)
+
+const uno = "hola"
+const dos = "Hola"
+const comparacion = uno === dos // >, >=, <, <=, ===, !==
+console.log(comparacion)
