7bde2da2c9a4377548aa1c15cc4b133528314b1c
aulas <aulas@SVP408D.academico.icesi.edu.co>
2024-04-03 08:07:39 -0500

    Calculator TODO

--- 2024-1/clase11-js-arrays/calculator.js
+++ 2024-1/clase11-js-arrays/calculator.js
@@ -1,6 +1,7 @@
 
 const numbers = [1,2,3,4,5,6,7,8,9,0]
 const operations = ["/","X","-","+","="]
+var result = 0
 
 function createNumbers() {
     const container = document.getElementById("numbers-container")
@@ -19,12 +20,25 @@ function createOperations() {
 }
 
 function onNumberClicked(number) {
+    // TODO: Manejar caso de placeholder
     const result = document.getElementById("result")
     result.innerHTML += number
 }
 
 function onOperationClicked(operation) {
-    console.log(operation)
+    // Obtener el valor del numero escrito
+    const h1 = document.getElementById("result")
+    const value = Number(h1.innerHTML)
+    // Determinar si la operacion es suma o igual
+    if(operation === "+") {
+        // Sumar
+        result = result + value
+        h1.innerHTML = ""
+    } else if(operation === "=") {
+        // TODO: Mostrar resultado sin el bug
+        h1.innerHTML = result
+    }
+    // TODO: Manejar los demas casos
 }
 
 function start() {
