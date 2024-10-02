7e9e1194373e750c10cae24ab77537d7fd1a0609
Santiago Martin Moreno Benavides <1144072657@u.icesi.edu.co>
2024-10-02 08:13:45 -0500

    Class and data

---
+++ 2024-2/11-objects/Product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, title, price, description, images, categoryName) {
+        this.id = id
+        this.title = title
+        this.price = price
+        this.description = description
+        this.images = images
+        this.categoryName = categoryName
+    }
+}
